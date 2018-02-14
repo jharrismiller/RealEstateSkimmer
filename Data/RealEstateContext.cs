ee0f4ccf9d6b262f9b2d726c034debb79b46a36c
John Miller <john.miller@bamtech.net>
2018-02-13 21:08:38 -0500

    In Progress

---
+++ Data/RealEstateContext.cs
@@ -0,0 +1,16 @@
+﻿using Common;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Data
+{
+    public class RealEstateContext : DbContext, IRealEstateContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb);Database=RealEstateAnalysis;Trusted_Connection=True;");
+        }
+
+
+    }
+}
